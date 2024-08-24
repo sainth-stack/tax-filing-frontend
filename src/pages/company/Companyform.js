4970a941c8ea68eb4a9e41bb67f924f3e7086785
Sainath Guraka <sainath.guraka@3insys.com>
2024-08-24 10:28:26 +0530

    updated

--- src/pages/company/Companyform.js
+++ src/pages/company/Companyform.js
@@ -218,7 +218,7 @@ const CompanyForm = ({
             onClick={companyId ? handleUpdate : handleSubmit}
             className="px-4 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
           >
-            {companyId ? "View" : "Save"}
+            {companyId ? "Update" : "Save"}
           </button>}
           <button
             onClick={() => {
