7f4c7fa5234908d09585f92337809a72554f0c5b
VishnuVardhanBonala-web4U <rvardhancm08@gmail.com>
2024-10-03 09:53:45 +0530

    "done"

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,7 +7,7 @@ import Topmenu from "../Menus/Topmenu";
 
 const Layout = ({ children }) => {
   const user = JSON.parse(localStorage.getItem('user'))
-  console.log(user)
+  //console.log(user)
   return (
     <>
       <div className="flex flex-col h-screen">
@@ -21,7 +21,7 @@ const Layout = ({ children }) => {
         </header>
         <div className="flex flex-grow" style={{}}>
           <aside className="w-25 bg-gray-100 shadow-md h-full fixed left-0 pt-16">
-            <Sidemenu {...{user}} />
+            <Sidemenu {...{ user }} />
           </aside>
           <div
             className="flex flex-col flex-grow"
